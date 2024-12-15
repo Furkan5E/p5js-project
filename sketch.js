d71112e7a48ec13e212ce338150bc75ab3030063
furkan5e <furkankeles10@hotmail.com>
2024-12-15 23:29:20 +0000

    fixed wrong commit on wrong repository

--- sketch.js
+++ sketch.js
@@ -8,26 +8,35 @@ let player = {
 let score = 0;
 let gameOver = false;
 
+let bullets = [];
+
 function setup() {
   createCanvas(900, 600);
 }
 
 function draw() {
-  background("#b8b4b4");
-  strokeWeight(3.5);
-  fill(255, 255, 255);
-  ellipse(player.x, player.y, 20 * 2);
+  drawPlayer();
   drawGun();
+  drawBullets();
   movePlayer();
 }
 
+function drawPlayer() {
+  background("#b8b4b4");
+  strokeWeight(3.5);
+  fill(255, 255, 255);
+  ellipse(player.x, player.y, 40);
+}
+
 function drawGun() {
+  push();
   translate(player.x, player.y);
   rotate(atan2(mouseY - player.y, mouseX - player.x));
 
   fill("#343434");
   strokeWeight(2);
   rect(21.7, - 5, 25.5, 10);
+  pop();
 }
 
 function movePlayer() {
@@ -43,4 +52,23 @@ function movePlayer() {
   if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
     player.y += player.speed;
   }
+}
+
+function drawBullets() {
+  for (let i = bullets.length-1; i >=0; i--) {
+    bullet = bullets[i];
+    strokeWeight(0);
+    fill("yellow");
+    bullet.x += 10
+    rect(bullet.x, bullet.y, 10.5, 5.5);
+  }
+}
+
+function mousePressed() {
+  let bullet = {
+    x: player.x,
+    y: player.y
+  }
+
+  bullets.push(bullet);
 }
